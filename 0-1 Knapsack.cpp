522027499d8f951468a8abaf43dc70a352d09589
Nayan singhai <nayansinghai66@gmail.com>
2020-03-24 16:35:53 +0530

    Add files via upload

---
+++ 0-1 Knapsack.cpp
@@ -0,0 +1,61 @@
+1. int stoi(string)
+-  long stol(string)
+-  long long stoll(string) 
+
+if stoi goes out of range
+
+2.a stringstream(default space separated)
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+int countWords(string str) 
+{ 
+    // breaking input into word using string stream 
+    stringstream s(str); // Used for breaking words 
+    string word; // to store individual words 
+  
+    int count = 0; 
+    while (s >> word) 
+        count++; 
+    return count; 
+} 
+  
+// Driver code 
+int main() 
+{ 
+    string s = "geeks for geeks geeks "
+               "contribution placements"; 
+    cout << " Number of words are: " << countWords(s); 
+    return 0; 
+} 
+
+2.b stringstream(any delimiter separated)
+
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+int countWords(string str) 
+{ 
+    // breaking input into word using string stream 
+    stringstream s(str); // Used for breaking words 
+    string word; // to store individual words 
+  
+    int count = 0; 
+   while( s.good() )
+{
+    string substr;
+    getline( s, substr, ',' );
+    cout<<substr<<endl;
+    count++;
+}
+    return count; 
+} 
+  
+// Driver code 
+int main() 
+{ 
+    string s = "geeks,for,geeks,geeks,"
+               "contribution,placements"; 
+    cout << " Number of words are: " << countWords(s); 
+    return 0; 
+} 
